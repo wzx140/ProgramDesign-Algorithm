e0e1aa70da735920ad00b36f54f37cb185467c6f
masterwangzx <masterwangzx@gmail.com>
2019-10-15 22:09:31 +0800

    a week2

---
+++ AlgorithmBasis/week2/q1.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 编程题＃1： 画家问题
+
+int guess(int wall[][17], int paint[][17], int size) {
+	int n = size;
+
+	// 根据wall第1行和paint数组, 计算paint其他行的值
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j < n + 1; j++) {
+
+			paint[i + 1][j] = (wall[i][j] + paint[i][j] + paint[i][j - 1] + paint[i][j + 1] + paint[i - 1][j]) % 2;
+
+		}
+	}
+
+	// 判断最后一行
+	for (int i = 1; i < n + 1; i++) {
+		if ((paint[n][i] + paint[n][i - 1] + paint[n][i + 1] + paint[n - 1][i]) % 2 != wall[n][i]) {
+			return n * n + 1;
+		}
+	}
+
+	// 计算粉刷次数
+	int count = 0;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			if (paint[i][j] == 1) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
+
+//枚举第一行的所有情况  
+int enumerate(int wall[][17], int paint[][17], int size) {
+	int n = size;
+	int minCount = n * n + 1;
+
+	while (paint[1][n + 1] < 1) {
+		int count = guess(wall, paint, size);
+		if (count < minCount) {
+			minCount = count;
+		}
+		paint[1][1]++;
+		int c = 1;
+		while (paint[1][c] > 1) {
+			paint[1][c] = 0;
+			c++;
+			paint[1][c]++;
+		}
+	}
+	return minCount;
+}
+
+int main(int argc, char *argv[]) {
+
+	// 初始化
+	int wall[16][17] = { 0 };
+	int paint[16][17] = { 0 };
+
+	// 读取cases数
+	int cases = 0;
+	cin >> cases;
+
+	for (int i = 0; i < cases; i++) {
+		// 读取case的size
+		int n = 0;
+		cin >> n;
+
+		// 初始化墙和粉刷 
+		for (int r = 0; r < 16; r++) {
+			for (int c = 0; c < 17; c++) {
+				wall[r][c] = 0;
+				paint[r][c] = 0;
+			}
+		}
+
+		// 读入wall
+		for (int r = 1; r < n + 1; r++) {
+			for (int c = 1; c < n + 1; c++) {
+				char ch;
+				cin >> ch;
+				wall[r][c] = ch == 'y' ? 0 : 1;
+			}
+		}
+
+		int minCount = enumerate(wall, paint, n);
+		if (minCount > n*n) {
+			cout << "inf" << endl;
+		} else {
+			cout << minCount << endl;
+		}
+	}
+	return 0;
+}
---
+++ AlgorithmBasis/week2/q2.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// 编程题＃2： 拨钟问题
+
+// 每个action都有4中实施方法，实施0-4次，所以总共有4^9种情况
+// 仔细观察actiongs数组发现，a4,a5,a6唯一的改变第一行的元素，类比第一个问题，我们可以得到以下解决方案
+// 对a1,a2,a3进行枚举，确定a4,a5,a6使得ABC都为12点
+// 检查是否存在a6,a8使得DEF都为12点，检查是否存在a7使得GHI都为12点
+int actions[9][9] = {
+	//   A  B  C  D  E  F  G  H  I
+		{1, 1, 0, 1, 1, 0, 0, 0, 0},  //a1: ABDE
+		{1, 1, 1, 0, 0, 0, 0, 0, 0},  //a2: ABC
+		{0, 1, 1, 0, 1, 1, 0, 0, 0},  //a3: BCEF
+		{1, 0, 0, 1, 0, 0, 1, 0, 0},  //a4: ADG
+		{0, 1, 0, 1, 1, 1, 0, 1, 0},  //a5: BDEFH
+		{0, 0, 1, 0, 0, 1, 0, 0, 1},  //a6: CFI
+		{0, 0, 0, 1, 1, 0, 1, 1, 0},  //a7: DEGH
+		{0, 0, 0, 0, 0, 0, 1, 1, 1},  //a8: GHI
+		{0, 0, 0, 0, 1, 1, 0, 1, 1}  //a9: EFHI
+};
+
+// clock经过time次的op拨动后
+void rotate(int clock[], int op, int time) {
+
+	for (int i = 0; i < 9; i++) {
+		clock[i] += actions[op][i] * time;
+		clock[i] %= 4;
+	}
+}
+
+bool guess(int clock[], int ops[]) {
+
+	// 储存中间状态
+	int tempClock[9] = { 0 };
+	for (int i = 0; i < 9; i++) {
+		tempClock[i] = clock[i];
+	}
+
+	// 应用a1 a2 a3更改
+	for (int i = 0; i < 3; i++) {
+		rotate(tempClock, i, ops[i]);
+	}
+
+	// 调整a4 a5 a6 以适应第一行
+	for (int i = 0; i < 3; i++) {
+		ops[i + 3] = (4 - tempClock[i]) % 4;
+	}
+
+	// 应用a4 a5 a6更改
+	for (int i = 3; i < 6; i++) {
+		rotate(tempClock, i, ops[i]);
+	}
+
+	// 调整a7 a9 以适应第二行
+	ops[6] = (4 - tempClock[3]) % 4;
+	ops[8] = (4 - tempClock[5]) % 4;
+	// 应用
+	rotate(tempClock, 6, ops[6]);
+	rotate(tempClock, 8, ops[8]);
+
+	// 检查第二行是否合法
+	if (tempClock[4] != 0) {
+		return false;
+	}
+
+	// 调整并应用a8
+	ops[7] = (4 - tempClock[6]) % 4;
+	rotate(tempClock, 7, ops[7]);
+
+	// 检查第三行是否合法
+	for (int i = 6; i < 9; i++) {
+		if (tempClock[i] != 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+//枚举a1 a2 a3的所有情况  
+int enumerate(int clock[], int ops[]) {
+
+	int minCount = pow(4, 9) + 1;
+	int tempOps[9] = { 0 };
+	for (int i = 0; i < 9; i++) {
+		tempOps[i] = ops[i];
+	}
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+
+				tempOps[0] = i;
+				tempOps[1] = j;
+				tempOps[2] = k;
+
+				if (guess(clock, tempOps)) {
+
+					// 统计移动次数
+					int count = 0;
+					for (int i = 0; i < 9; i++) {
+						count += tempOps[i];
+					}
+
+					if (count < minCount) {
+						for (int i = 0; i < 9; i++) {
+							ops[i] = tempOps[i];
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+	return minCount;
+}
+
+//int main(int argc, char *argv[]) {
+//
+//	int clock[9] = { 0 };
+//	// 每种操作的次数
+//	int ops[9] = { 0 };
+//
+//	for (int i = 0; i < 9; i++) {
+//		cin >> clock[i];
+//	}
+//
+//	int count = enumerate(clock, ops);
+//
+//	for (int i = 0; i < 9; i++) {
+//		if (ops[i] != 0) {
+//			cout << i + 1 << ' ';
+//			ops[i]--;
+//			i--;
+//		}
+//	}
+//
+//	return 0;
+//}
