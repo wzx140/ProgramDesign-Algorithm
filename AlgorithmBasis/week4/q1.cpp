08b8f55eab436312bf6c889b4bab36a2135b8eab
masterwangzx <masterwangzx@gmail.com>
2019-10-22 19:45:22 +0800

    a week4

---
+++ AlgorithmBasis/week4/q1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+// 1：集合加法
+// 哈希查找，将集合的数以 值-该值的数量 的方式存入哈希表
+// 遍历某个集合a，在另一个集合中查找 sum - a[i]，单个查找的时间复杂度为 O(1)
+// 如果存在重复的值，是不适用哈希查找的，但本题只需返回符合要求的个数，并不要求返回两个数的索引
+
+int search(map<int, int>& a, map<int, int>& b, int sum) {
+	int count = 0;
+
+	if (a.size() < b.size()) {
+		map<int, int>::iterator iter = a.begin();
+		while (iter != a.end()) {
+			if (b.find(sum - iter->first) != b.end()) {
+				// 两个集合符合要求的值的个数乘积
+				count += iter->second * b[sum - iter->first];
+			}
+			iter++;
+		}
+	} else {
+		map<int, int>::iterator iter = b.begin();
+		while (iter != b.end()) {
+			if (a.find(sum - iter->first) != a.end()) {
+				// 两个集合符合要求的值的个数乘积
+				count += iter->second * a[sum - iter->first];
+			}
+			iter++;
+		}
+	}
+
+	return count;
+}
+
+int main(int argc, char *argv[]) {
+	int n = 0;
+	cin >> n;
+
+	while (n--) {
+		int sum = 0;
+		cin >> sum;
+
+		int setLength = 0;
+
+		// 集合A
+		cin >> setLength;
+		map<int,int> a;
+		while (setLength--) {
+			int num = 0;
+			cin >> num;
+			a[num]++;
+		}
+
+		// 集合B
+		cin >> setLength;
+		map<int,int> b;
+		while (setLength--) {
+			int num = 0;
+			cin >> num;
+			b[num]++;
+		}
+
+		cout << search(a, b, sum) << endl;
+
+	}
+	return 0;
+}
---
+++ AlgorithmBasis/week4/q2.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 2： 滑雪
+
+struct Dot {
+	int x;
+	int y;
+	int height;
+};
+
+// 最短路径缓存数组
+int route[101][101];
+// 雪场
+int playground[101][101];
+int row = 0;
+int col = 0;
+
+// 搜索临近四个区域，这里采用课程里介绍的"人人为我"策略
+int search(int x, int y) {
+
+	if (route[x][y] > 0) {
+		return route[x][y];
+	}
+
+	int nextX = 0;
+	int nextY = 0;
+	int maxRoute = 0;
+
+	// 上
+	nextX = x;
+	nextY = y - 1;
+	if (nextY >= 0) {
+		if (playground[nextX][nextY] < playground[x][y]) {
+			maxRoute = max(maxRoute, route[nextX][nextY]);
+		}
+	}
+
+	// 下
+	nextX = x;
+	nextY = y + 1;
+	if (nextY < col) {
+		if (playground[nextX][nextY] < playground[x][y]) {
+			maxRoute = max(maxRoute, route[nextX][nextY]);
+		}
+	}
+
+	// 左
+	nextX = x - 1;
+	nextY = y;
+	if (nextX >= 0) {
+		if (playground[nextX][nextY] < playground[x][y]) {
+			maxRoute = max(maxRoute, route[nextX][nextY]);
+		}
+	}
+
+	// 右
+	nextX = x + 1;
+	nextY = y;
+	if (nextX < row) {
+		if (playground[nextX][nextY] < playground[x][y]) {
+			maxRoute = max(maxRoute, route[nextX][nextY]);
+		}
+	}
+
+	route[x][y] = maxRoute + 1;
+	return maxRoute + 1;
+}
+
+bool cmp(const Dot& a, const Dot& b) {
+	return a.height < b.height;
+}
+
+int main(int argc, char *argv[]) {
+
+	cin >> row >> col;
+
+	// 初始化雪场
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			int value = 0;
+			cin >> value;
+			playground[i][j] = value;
+		}
+	}
+
+	// 排序
+	// 这里有点麻烦,需要再新建一个数组储存排序结果
+	vector<Dot> beSort;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			Dot dot = { i,j,playground[i][j] };
+			beSort.push_back(dot);
+		}
+	}
+	sort(beSort.begin(), beSort.end(), cmp);
+
+	// 搜索
+	int res = 0;
+	for (int i = 0; i < beSort.size(); i++) {
+		int x = beSort[i].x;
+		int y = beSort[i].y;
+		res = max(res, search(x, y));
+	}
+
+	cout << res << endl;
+
+	return 0;
+}
