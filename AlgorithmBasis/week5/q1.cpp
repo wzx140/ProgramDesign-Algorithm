8190ca855fa09bd79cf40f14842c5c305ee28588
masterwangzx <masterwangzx@gmail.com>
2019-10-23 11:38:04 +0800

    a week5

---
+++ AlgorithmBasis/week5/q1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 1：UNIMODAL PALINDROMIC DECOMPOSITIONS
+// 比较难想到啊
+// 参考这篇的递推公式 http://blog.sina.com.cn/s/blog_7223fd910100x2bw.html
+
+// 如果使用int会溢出
+unsigned int cache[250][250];
+
+int main(int argc, char *argv[]) {
+	int n = 0;
+
+	// 缓存数组预处理,边界条件
+	for (int i = 0; i < 250; i++) {
+		for (int j = 0; j < 250; j++) {
+			if (i == 0) {
+				cache[i][j] = 1;
+			} else if (j > i) {
+				cache[i][j] = 0;
+			} else {
+				cache[i][j] = 1;
+			}
+		}
+	}
+
+	while (cin >> n && n != 0) {
+
+		if (cache[n][1] == 1) {
+			for (int i = 2; i <= n; i++) {
+				for (int j = i / 2; j >= 1; j--) {
+					cache[i][j] = cache[i - 2 * j][j] + cache[i][j + 1];
+				}
+			}
+		}
+
+		cout << n << ' ' << cache[n][1] << endl;
+	}
+
+	return 0;
+}
---
+++ AlgorithmBasis/week5/q2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 2： Charm Bracelet
+
+int weights[3402];
+int charms[3402];
+int cache[12881];
+int limit;
+int n;
+
+// 递归的写法,较为容易理解
+//int getMax(int item, int limit) {
+//	if (item == 0) {
+//		return 0;
+//	}
+//	if (weights[item] > limit) {
+//		// 最后一个手镯超重
+//		return getMax(item - 1, limit);
+//	} else {
+//		// 最后一个手镯放与不放,返回最大的charm
+//		return max(getMax(item - 1, limit), getMax(item - 1, limit - weights[item]) + weights[item]);
+//	}
+//}
+
+int main(int argc, char *argv[]) {
+	cin >> n >> limit;
+
+	// 输入
+	for (int i = 0; i < n; i++) {
+		cin >> weights[i] >> charms[i];
+	}
+
+	int currLimit = 0;
+	for (int i = 0; i < n; i++) {
+		int weight = weights[i];
+		int desirability = charms[i];
+
+		// 剪枝，减少不必要的遍历
+		currLimit += weight;
+		currLimit = min(currLimit, limit);
+
+		// 倒序,因为防止出现 cache[j - weight]中已经使用了当前手镯 的情况
+		for (int j = limit; j >= weight; j--) {
+			// 当前手镯应不应该放入
+			cache[j] = max(cache[j - weight] + desirability, cache[j]);
+		}
+	}
+
+	cout << cache[limit] << endl;
+
+
+	return 0;
+}
