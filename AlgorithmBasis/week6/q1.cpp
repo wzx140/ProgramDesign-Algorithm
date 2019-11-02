61b8d02c6870628b8389f6d81ec20c485876c0b2
masterwangzx <masterwangzx@gmail.com>
2019-11-02 16:49:27 +0800

    a week6

---
+++ AlgorithmBasis/week6/q1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+// 1： 棋盘问题
+// 深度优先，动归
+
+bool board[8][8];	// 棋盘
+bool cols[8];		// 列状态，每列是否放过棋子
+int n = 0;			// 棋盘大小
+int k = 0;			// 棋子个数
+int count = 0;		// 方案数
+int chess = 0;		// 已放棋子数
+
+// 从i行开始的递归深搜
+// 因为每个棋子在行上不可能重复,所以要储存列的状态，
+void DFS(int i) {
+	// 找到一种方法
+	if (chess == k) {
+		count++;
+		return;
+	}
+
+	if (i >= n) {
+		return;
+	}
+
+	// 列搜索
+	for (int j = 0; j < n; j++) {
+		if (!board[i][j] && !cols[j]) {
+			cols[j] = true;
+			chess++;
+			DFS(i + 1);
+			// 恢复状态
+			cols[j] = false;
+			chess--;
+		}
+	}
+
+	// 下一行
+	// 允许出现空行的情形
+	DFS(i + 1);
+}
+
+int main(int argc, char *argv[]) {
+
+	while ((cin >> n, cin >> k) && n != -1 && k != -1) {
+		for (int i = 0; i < n; i++) {
+			cols[i] = false;
+		}
+		count = 0;
+		chess = 0;
+
+		// 初始化棋盘
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				char ch;
+				cin >> ch;
+				board[i][j] = ch == '.';
+			}
+		}
+
+		DFS(0);
+		cout << count << endl;
+	}
+
+	return 0;
+}
---
+++ AlgorithmBasis/week6/q2.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// 2：A Knight's Journey
+// 简单的翻译一下，骑士在矩形棋盘上走，一步只能直走两格再拐一个弯(垂直方向走一格)
+// 在A1起点下，棋盘每个点都只经过一次的方案
+
+// A1 坐标为(0,0) 右与下为正方向
+bool square[26][26];	// 棋盘
+
+int p = 0;	// x,行数，数字表示
+int q = 0;	// y,列数，字母表示
+int maxStep = 0;	// 总步数
+bool flag = false;	//	已经找到方案
+
+struct Node {
+	int x = 0;
+	int y = 0;
+}route[26];
+
+// 方向：0~7
+// ！题目要求字典序最小，所以这里要按序排列
+void move(int& x, int& y, int direction) {
+	switch (direction) {
+	case 0:
+		x -= 2;
+		y -= 1;
+		break;
+	case 1:
+		x -= 2;
+		y += 1;
+		break;
+	case 2:
+		x -= 1;
+		y -= 2;
+		break;
+	case 3:
+		x -= 1;
+		y += 2;
+		break;
+	case 4:
+		x += 1;
+		y -= 2;
+		break;
+	case 5:
+		x += 1;
+		y += 2;
+		break;
+	case 6:
+		x += 2;
+		y -= 1;
+		break;
+	case 7:
+		x += 2;
+		y += 1;
+		break;
+	default:
+		break;
+	}
+}
+
+void DFS(int x, int y, int step) {
+
+	if (flag) {
+		// 在找到字典序最小的方案后快速结束其他正在递归中的函数
+		// 避免不必要的递归
+		return;
+	}
+
+	square[x][y] = true;
+	route[step].x = x;
+	route[step].y = y;
+	step++;
+
+	if (step == maxStep) {
+		flag = true;
+		return;
+	}
+
+	for (int i = 0; i < 8; i++) {
+		int nextX = x;
+		int nextY = y;
+		move(nextX, nextY, i);
+
+		if (!square[nextX][nextY] &&
+			nextX >= 0 && nextX < q &&
+			nextY >= 0 && nextY < p) {
+
+			DFS(nextX, nextY, step);
+		}
+	}
+
+	// 恢复状态
+	square[x][y] = false;
+}
+
+int main(int argc, char *argv[]) {
+	int n = 0;
+	cin >> n;
+
+	for (int index = 1; index <= n; index++) {
+		cin >> p >> q;
+		maxStep = p * q;
+		flag = false;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				square[i][j] = false;
+			}
+		}
+
+		DFS(0, 0, 0);
+
+		cout << "Scenario #" << index << ":" << endl;
+		if (flag) {
+			for (int i = 0; i < maxStep; i++) {
+				char ch = route[i].x + 'A';
+				cout << ch << route[i].y + 1;
+			}
+			cout << endl << endl;
+		} else {
+			cout << "impossible" << endl << endl;
+		}
+	}
+
+	return 0;
+}
