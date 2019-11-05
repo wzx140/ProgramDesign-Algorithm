892432f8883d98cc2f87966366c1f699f50caac1
masterwangzx <masterwangzx@gmail.com>
2019-11-05 20:56:51 +0800

    a week8

---
+++ AlgorithmBasis/week8/q1.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <queue>
+#include <bitset>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::queue;
+using std::bitset;
+
+// 1：Flip Game
+// 在4x4的方格上，翻转棋子(黑->白，白->黑)
+// 每次邻近的四个棋子也会被翻动(如果有的话)
+// 最短几次可以全黑或者全白
+
+// unsigned short类型 16位，正好可以存储16个格子的状态
+
+struct State {
+	unsigned short board;
+	int count;
+	State(unsigned short tBoard, int tCount) :board(tBoard), count(tCount) {}
+};
+
+bool getPiece(unsigned short board, int i, int j) {
+	return !((board & (1 << i * 4 + j)) == 0);
+}
+
+// test
+//void print(unsigned short board) {
+//	for (int i = 0; i < 4; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			bool piece = getPiece(board, i, j);
+//			if (piece) {
+//				cout << 'w';
+//			} else {
+//				cout << 'b';
+//			}
+//		}
+//		cout << endl;
+//	}
+//}
+
+void flipOne(unsigned short& board, int i, int j) {
+	if (getPiece(board, i, j)) {
+		board -= 1 << i * 4 + j;
+	} else {
+		board += 1 << i * 4 + j;
+	}
+}
+
+unsigned short flip(unsigned short board, int i, int j) {
+	flipOne(board, i, j);
+
+	if (i > 0) {
+		flipOne(board, i - 1, j);
+	}
+
+	if (i < 3) {
+		flipOne(board, i + 1, j);
+	}
+
+	if (j > 0) {
+		flipOne(board, i, j - 1);
+	}
+
+	if (j < 3) {
+		flipOne(board, i, j + 1);
+	}
+
+	return board;
+}
+
+bool finish(unsigned short board) {
+	bool piece = getPiece(board, 0, 0);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (getPiece(board, i, j) != piece) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+int BFS(unsigned short board) {
+	queue<State> open;
+	bitset< 1 << 16 > aClose;
+
+	// 放入初始棋盘
+	State state(board, 0);
+	open.push(state);
+
+	while (!open.empty()) {
+		State aState = open.front();
+		open.pop();
+		unsigned short aBoard = aState.board;
+		int count = aState.count;
+		aClose[aBoard] = true;
+
+		if (finish(aBoard)) {
+			return count;
+		} else {
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					unsigned short temp = flip(aBoard, i, j);
+					// 判重
+					if (!aClose[temp]) {
+						State nextState(temp, count + 1);
+						open.push(nextState);
+					}
+
+				}
+			}
+		}
+	}
+
+	return -1;
+}
+
+int main(int argc, char *argv[]) {
+
+	unsigned short board = 0;	// 棋盘
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			char ch;
+			cin >> ch;
+			if (ch == 'w') {
+				board += (1 << (i * 4 + j));
+			}
+		}
+	}
+
+	int res = BFS(board);
+
+	if (res == -1) {
+		cout << "Impossible"<<endl;
+	} else {
+		cout << res << endl;
+	}
+
+	return 0;
+}
---
+++ AlgorithmBasis/week8/q2.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+#include <queue>
+#include <bitset>
+#include <vector>
+
+using std::endl;
+using std::cin;
+using std::cout;
+using std::queue;
+using std::bitset;
+using std::vector;
+
+// 2：迷宫问题
+
+// 迷宫
+int maze;
+
+struct State {
+	short x = 0;		// 当前位置
+	short y = 0;
+	int father = -1;	// 父结点
+	int board;			// 当前已走过的状态
+};
+
+// 状态数组
+State states[1 << 25];
+// 状态是否走过
+bitset<1 << 25> aClose;
+
+// 某点是否走过
+bool getPoint(int board, int i, int j) {
+	return !((board & (1 << i * 5 + j)) == 0);
+}
+
+// test
+//void print(int board) {
+//	for (int i = 0; i < 5; i++) {
+//		for (int j = 0; j < 5; j++) {
+//			bool point = getPoint(board, i, j);
+//			if (point) {
+//				cout << 1 << ' ';
+//			} else {
+//				cout << 0 << ' ';
+//			}
+//		}
+//		cout << endl;
+//	}
+//}
+
+int nextState(int board, int i, int j, int op) {
+	switch (op) {
+	case 0:
+		// 上移
+		if (j > 0 && !getPoint(board, i, j - 1)) {
+			return board + (1 << i * 5 + j - 1);
+		} else {
+			return -1;
+		}
+		break;
+
+	case 1:
+		// 下移
+		if (j < 4 && !getPoint(board, i, j + 1)) {
+			return board + (1 << i * 5 + j + 1);
+		} else {
+			return -1;
+		}
+		break;
+
+	case 2:
+		// 左移
+		if (i > 0 && !getPoint(board, i - 1, j)) {
+			return board + (1 << (i - 1) * 5 + j);
+		} else {
+			return -1;
+		}
+		break;
+
+	case 3:
+		// 右移
+		if (i < 4 && !getPoint(board, i + 1, j)) {
+			return board + (1 << (i + 1) * 5 + j);
+		} else {
+			return -1;
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+int DBFS(int init) {
+	queue<int> open;
+
+	states[init].board = init;
+	open.push(init);
+
+	while (!open.empty()) {
+		int board = open.front();
+		open.pop();
+		aClose[board] = true;
+
+		if (getPoint(board, 4, 4)) {
+			// 走到右下角了
+			return board;
+
+		} else {
+			int x = states[board].x;
+			int y = states[board].y;
+
+			for (int op = 0; op < 4; op++) {
+				int res = nextState(board, x, y, op);
+
+				if (res != -1 && !aClose[res]) {
+					open.push(res);
+
+					// 更新父指针
+					states[res].father = board;
+					// 更新坐标
+					switch (op) {
+					case 0:
+						// 上移
+						states[res].x = x;
+						states[res].y = y - 1;
+						break;
+
+					case 1:
+						// 下移
+						states[res].x = x;
+						states[res].y = y + 1;
+						break;
+
+					case 2:
+						// 左移
+						states[res].x = x - 1;
+						states[res].y = y;
+						break;
+
+					case 3:
+						// 右移
+						states[res].x = x + 1;
+						states[res].y = y;
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			int value = 0;
+			cin >> value;
+			if (value == 1) {
+				maze += 1 << i * 5 + j;
+			}
+		}
+	}
+
+	// 返回路径终点状态
+	int board = DBFS(maze);
+
+	// 方向寻找到初始状态
+	vector<int> result;
+	while (board != -1) {
+		result.push_back(board);
+		board = states[board].father;
+	}
+	// 输出
+	for (int i = result.size() - 1; i >= 0; i--) {
+		int x = states[result[i]].x;
+		int y = states[result[i]].y;
+		cout << '(' << x << ", " << y << ')' << endl;
+	}
+
+	return 0;
+}
